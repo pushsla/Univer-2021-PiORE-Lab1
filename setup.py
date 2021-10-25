16fe38f7e28db9ecafa33350db93088d7644fa6d
pushsla <push2001sla@gmail.com>
2021-10-25 12:27:54 +0500

    v1.0: dependencies setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name='funniest',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+setup(name='lab1-data-preparation.py',
+      version='1.0',
+      description='Generate and investigate randoms',
+      url='https://github.com/pushsla/Univer-2021-PiORE-Lab1',
+      author='pushsla',
+      author_email='push2001sla@gmail.com',
       license='MIT',
-      packages=['funniest'],
+      packages=['lab1-data-preparation.py'],
       install_requires=[
-          'markdown',
+            'matplotlib',
+            'PyQt5',
       ],
       zip_safe=False)
